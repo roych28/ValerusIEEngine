470a3438489a2d74800affb6d6f44604c107e9d1
DESKTOP-R361AMU\roych <roych28@gmail.com>
2018-02-20 15:34:45 +0200

    connect browser action to page load

--- ChromeExtension/app/background.js
+++ ChromeExtension/app/background.js
@@ -1,11 +1,16 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.command == "connect")
-	
-		var url = "main.html?url="
-        + encodeURIComponent(request.url);
-	
-		chrome.tabs.create({'url': url}, function(tab) {
-			console.log("Valerus tab loaded", tab.id);
-		});
-  });
+//chrome.runtime.onMessage.addListener(
+chrome.browserAction.onClicked.addListener(
+	function(){
+        chrome.storage.sync.get({
+            url: 'http://47.21.44.216/'
+        }, function(items) {
+            console.log("chrome open valerus tab url : " + items.url);
+
+            var url = "main.html?url="
+                + encodeURIComponent(items.url);
+
+            chrome.tabs.create({'url': url}, function (tab) {
+                console.log("Valerus tab loaded", tab.id);
+            });
+        });
+});
