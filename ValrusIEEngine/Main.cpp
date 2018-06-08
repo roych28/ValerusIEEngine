30bf9e472f5ffac32716357736db9797ea0dee73
DESKTOP-R361AMU\roych <roych28@gmail.com>
2018-06-08 23:20:05 +0300

    test app from ms

--- ValrusIEEngine/Main.cpp
+++ ValrusIEEngine/Main.cpp
@@ -1,5 +1,97 @@
 #include "stdafx.h"
-#include <Windows.h>
+#include <windows.h>
+#include <tchar.h>
+#include <exdisp.h> // IWebBrowser2
+
+HINSTANCE hInst;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+HINSTANCE hATLDLL;
+typedef BOOL(__stdcall *PAAWI)(void);
+PAAWI pAtlAxWinInit;
+typedef HRESULT(__stdcall *PAAGC) (HWND hWnd, IUnknown** pUnknown);
+PAAGC pAtlAxGetControl;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	hInst = hInstance;
+	hATLDLL = LoadLibrary(_T("atl.dll"));
+	pAtlAxWinInit = (PAAWI)GetProcAddress(hATLDLL, "AtlAxWinInit");
+	if (pAtlAxWinInit)
+		pAtlAxWinInit();
+	pAtlAxGetControl = (PAAGC)GetProcAddress(hATLDLL, "AtlAxGetControl");
+	WNDCLASSEX wcex =
+	{
+		sizeof(WNDCLASSEX), 0, WndProc, 0, 0, hInst, LoadIcon(NULL, IDI_APPLICATION),
+		LoadCursor(NULL, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1), NULL, TEXT("WindowClass"), NULL,
+	};
+	if (!RegisterClassEx(&wcex))
+		return MessageBox(NULL, L"Cannot register class !", L"Error", MB_ICONERROR | MB_OK);
+	int nX = (GetSystemMetrics(SM_CXSCREEN) - 1024) / 2, nY = (GetSystemMetrics(SM_CYSCREEN) - 800) / 2;
+	HWND hWnd = CreateWindowEx(0, wcex.lpszClassName, TEXT("Test"), WS_OVERLAPPEDWINDOW, nX, nY, 1024, 800, NULL, NULL, hInst, NULL);
+	if (!hWnd)
+		return MessageBox(NULL, L"Cannot create window !", L"Error", MB_ICONERROR | MB_OK);
+	ShowWindow(hWnd, SW_SHOWNORMAL);
+	UpdateWindow(hWnd);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hHTMLWindow;
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		hHTMLWindow = CreateWindow(_T("AtlAxWin"), _T("www.facebook.com"), WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL | WS_HSCROLL, 10, 10, 700, 100, hWnd, (HMENU)1000, hInst, 0);
+		if (hHTMLWindow)
+		{
+			HRESULT hr = E_FAIL;
+			IUnknown* pUnknown = NULL;
+			IWebBrowser2 *pWebBrowser = NULL;
+			hr = pAtlAxGetControl(hHTMLWindow, &pUnknown);
+			if (SUCCEEDED(hr))
+			{
+				hr = pUnknown->QueryInterface(__uuidof(IWebBrowser2), (void**)&pWebBrowser);
+				if (SUCCEEDED(hr))
+				{
+					// Test
+					/*
+					BSTR bstrURL;
+					VARIANT_BOOL vb;
+					bstrURL = SysAllocString(L"www.msdn.com");
+					VARIANT var;
+					var.vt = VT_EMPTY;
+					pWebBrowser->Navigate(bstrURL, &var, &var, &var, &var);
+					SysFreeString(bstrURL);
+					*/
+					pWebBrowser->Release();
+				}
+			}
+		}
+		return 0;
+	}
+	break;
+	case WM_SIZE:
+		MoveWindow(hHTMLWindow, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+		break;
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+/*#include <Windows.h>
 #include <tchar.h>
 #include <stdlib.h>
 #include <iostream>
